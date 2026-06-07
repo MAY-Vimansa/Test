d59362f0b96472e1c88a7e53b555c4113969b37b
MAY-Vimansa <mayvimansa@students.nsbm.ac.lk>
2026-06-07 17:36:54 +0530

    add a input field

--- name.java
+++ name.java
@@ -1,5 +1,10 @@
+java.util.Scanner
 public class name{
     public static void main(String arg[]){
-        System.out.println("yasith");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name=scanner.nextLine();
+        System.out.println(name);
+
     }
 }
